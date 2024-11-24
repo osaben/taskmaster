1ac7d8c7f9951b5171388fbea230f53bf1404111
eddynics <osareniyeosazee@gmail.com>
2024-11-24 22:41:57 +0100

    switching from monogodb to sqlite

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose'); // Or import PostgreSQL client
-const taskRoutes = require('./routes/taskRoutes');
-const authRoutes = require('./routes/authRoutes');
+// const mongoose = require('mongoose'); // Or import PostgreSQL client
+const taskRoutes = require('./route/taskRoutes');
+const authRoutes = require('./route/authRoutes');
 
 // Middleware
 app.use(express.json());
 
-// Routes
-app.use('/api/tasks', taskRoutes);
-app.use('/api/auth', authRoutes);
 
-// Database Connection (Replace with PostgreSQL config if needed)
-mongoose.connect('mongodb://localhost/taskmaster_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// Test Route
+app.get('/', (req, res) => {
+  res.send('Express.js Server is Running!');
 });
 
+// Routes
+ app.use('/api/tasks', taskRoutes);
+ app.use('/api/auth', authRoutes);
+
+// Database Connection (Replace with PostgreSQL config if needed)
+// mongoose.connect('mongodb://localhost/taskmaster_db', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
 app.listen(3000, () => console.log('Server running on port 3000'));
+
