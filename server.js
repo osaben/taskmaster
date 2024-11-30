f6d97d355e0180e50cae23f194d9e703de3a4450
eddynics <osareniyeosazee@gmail.com>
2024-11-30 23:04:24 +0100

    server.js make the main index file

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
 const path = require('path');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
 //app.use(express.static('public')); // Serving static files from 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html'); // Manually serve the index.html as a fallback
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/login.html'); // Manually serve the index.html as a fallback
+// });
 // Middleware
 app.use(bodyParser.json());
 
